e365550c756e9123dd4e5f269ac8e47558c18009
lscspirit <lscspirit@hotmail.com>
2015-11-21 03:15:19 +0800

    Completely remove the old novacast-sdk gem

--- novacast-sdk/lib/novacast-sdk/apis/event_v1/errors.rb
+++
@@ -1,8 +0,0 @@
-module Novacast
-  module API
-    module EventV1
-      class EventNotActiveError < Novacast::SDK::Error; DEFAULT_STATUS = :forbidden;    end
-      class NoEventAccessError  < Novacast::SDK::Error; DEFAULT_STATUS = :unauthorized; end
-    end
-  end
-end
--- novacast-sdk/lib/novacast-sdk/version.rb
+++
@@ -1,5 +0,0 @@
-module Novacast
-  module SDK
-    VERSION = '0.1.0'
-  end
-end
