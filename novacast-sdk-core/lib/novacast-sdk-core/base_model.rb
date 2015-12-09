fd41afa87a3e47c2b5b1c085b62a9ac676cfa4a4
lscspirit <lscspirit@hotmail.com>
2015-12-10 02:57:39 +0800

    New NovacastSDK::ModelSerializer to allow customizing how an object is serialized into SDK representation

--- novacast-sdk-core/lib/novacast-sdk-core/base_model.rb
+++ novacast-sdk-core/lib/novacast-sdk-core/base_model.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'novacast-sdk-core/model_serializer'
 
 module NovacastSDK
   class BaseModel
@@ -6,11 +7,33 @@ module NovacastSDK
       NovacastSDK
     end
 
+    #
+    # Class Methods
+    #
+
     def self.model_properties
       {}
     end
 
-    def initialize(obj)
+    def self.serializer=(serializer_klass)
+      raise ArgumentError, 'serializer class must be child class of Novacast::ModelSerializer' unless serializer_klass < NovacastSDK::ModelSerializer
+      @serializer_klass = serializer_klass
+    end
+
+    def self.serializer
+      @serializer_klass
+    end
+
+    #
+    # Instance
+    #
+
+    # Creates a new SDK model
+    # @param obj the object to create the model from
+    # @param [Hash] opts options
+    # @option opts [Class] :serializer the serializer class
+    def initialize(obj, opts = {})
+      self.inst_serializer = opts[:serializer] if opts[:serializer]
       build_object obj
     end
 
@@ -49,19 +72,24 @@ module NovacastSDK
     private
 
     def build_object(obj)
-      is_hash = obj.is_a?(Hash)
+      obj = _to_serializer obj
 
       self.class.model_properties.each_pair do |key, definition|
         type      = definition[:type]
         base_name = definition[:base_name]
 
-        prop_value = is_hash ? (obj[base_name.to_sym] || obj[base_name]) : obj.send(base_name)
+        prop_value = obj.send base_name
 
         if type =~ /^Array\[(.*)\]/i
           if prop_value.is_a?(Array)
             self.send "#{base_name}=", prop_value.map{ |v| _deserialize($1, v) }
+          elsif prop_value.respond_to?(:each)
+            # property is not an array, but responds to :each
+            arr_values = []
+            prop_value.each { |v| arr_values << _deserialize($1, v) }
+            self.send "#{base_name}=", arr_values
           else
-            # property is not an array
+            raise "property value is not an array for '#{base_name}'"
           end
         elsif !prop_value.nil?
           self.send "#{base_name}=", _deserialize(type, prop_value)
@@ -69,6 +97,24 @@ module NovacastSDK
           # property not found on object
         end
       end
+    end
+
+    def _to_serializer(obj)
+      klass = NovacastSDK::ModelSerializer
+
+      # There are three places that a serializer can be specified, and they are resolved with the priority below (from top to bottom):
+      # 1. :serializer option when instantiating the model (i.e. NovacastSDK::BaseModel#new)
+      # 2. A subclass with the name 'ModelSerializer' within the target object's class
+      # 3. Setting the serializer on the model class (i.e. NovacastSDK::BaseModel::serializer=)
+      if @inst_serializer
+        klass = @inst_serializer
+      elsif obj.class.const_defined?(:ModelSerializer) && (serializer = obj.class.const_get(:ModelSerializer)) < NovacastSDK::ModelSerializer
+        klass = serializer
+      elsif (serializer = self.class.serializer)
+        klass = serializer
+      end
+
+      klass.new(obj)
     end
 
     def _deserialize(type, value)
@@ -111,6 +157,13 @@ module NovacastSDK
       else
         value
       end
+    end
+
+    private
+
+    def inst_serializer=(klass)
+      raise ArgumentError, 'serializer class must be child class of Novacast::ModelSerializer' unless klass < NovacastSDK::ModelSerializer
+      @inst_serializer = klass
     end
   end
 end
