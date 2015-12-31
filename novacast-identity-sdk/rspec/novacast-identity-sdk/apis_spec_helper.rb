6963cdde7afb1eb5e5bcb265077a1859ae1484e4
lscspirit <lscspirit@hotmail.com>
2016-01-01 04:11:10 +0800

    Update the novacast-identity-sdk with the latest IdentityV1 definitions.

--- novacast-identity-sdk/rspec/novacast-identity-sdk/apis_spec_helper.rb
+++ novacast-identity-sdk/rspec/novacast-identity-sdk/apis_spec_helper.rb
@@ -2,66 +2,508 @@ require 'json'
 
 module NovacastSDK
   module IdentityV1
+    class Utils
+      def self.type_check(target, type_name)
+        NovacastSDK::Utils.type_check target, type_name do |model_name|
+          NovacastSDK::IdentityV1::Models.const_get(model_name)
+        end
+      end
+    end
+
+    class ApiRequests
+  
+      REQUESTS = { 
+        validate_token: {
+          
+          :'access_token' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_account: {
+          
+          :'info' => {
+            type: 'AccountRequest',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        sign_in: {
+          
+          :'info' => {
+            type: 'AccountRequest',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        sign_out: {
+          
+        }, 
+        get_account: {
+          
+          :'account_uid' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_account_permissions: {
+          
+          :'account_uid' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          },
+          
+          :'resources' => {
+            type: 'Array[String]',
+            path_param: false,
+            body_param: false,
+            required: false
+          }
+          
+        }, 
+        validate_account_permission: {
+          
+          :'resource' => {
+            type: 'String',
+            path_param: false,
+            body_param: false,
+            required: true
+          },
+          
+          :'permission' => {
+            type: 'String',
+            path_param: false,
+            body_param: false,
+            required: true
+          },
+          
+          :'account_uid' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_account_roles: {
+          
+          :'account_uid' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          },
+          
+          :'resources' => {
+            type: 'Array[String]',
+            path_param: false,
+            body_param: false,
+            required: false
+          }
+          
+        }, 
+        assign_account_role: {
+          
+          :'role' => {
+            type: 'AssignRoleRequest',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'account_uid' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_account_user_roles: {
+          
+          :'account_uid' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_account_user_role: {
+          
+          :'user_role' => {
+            type: 'AccessRoleResource',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'account_uid' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_domain: {
+          
+          :'domain' => {
+            type: 'DomainInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        get_domain_by_key: {
+          
+          :'domain_key' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_domain: {
+          
+          :'domain_id' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_access_permission: {
+          
+          :'permission' => {
+            type: 'AccessPermissionInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        update_access_permission: {
+          
+          :'permission' => {
+            type: 'AccessPermissionInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'permission_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        delete_access_permission: {
+          
+          :'permission_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_access_resource: {
+          
+          :'resource' => {
+            type: 'AccessResourceInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        update_access_resource: {
+          
+          :'resource' => {
+            type: 'AccessResourceInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'resource_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        delete_access_resource: {
+          
+          :'resource_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        delete_role_permission: {
+          
+          :'role_permission_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_role_set: {
+          
+          :'role_set' => {
+            type: 'RoleSetInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          }
+          
+        }, 
+        get_role_set_by_key: {
+          
+          :'role_set_key' => {
+            type: 'String',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_role_set: {
+          
+          :'role_set_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_roles: {
+          
+          :'role_set_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_role: {
+          
+          :'role' => {
+            type: 'AccessRoleInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'role_set_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        update_role: {
+          
+          :'role' => {
+            type: 'AccessRoleInfo',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'role_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        delete_access_role: {
+          
+          :'role_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        get_role_permissions: {
+          
+          :'role_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        create_role_permission: {
+          
+          :'role_permission' => {
+            type: 'RolePermission',
+            path_param: false,
+            body_param: true,
+            required: true
+          },
+          
+          :'role_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        }, 
+        delete_user_role: {
+          
+          :'user_role_id' => {
+            type: 'Integer',
+            path_param: true,
+            body_param: false,
+            required: true
+          }
+          
+        } 
+      }
+  
+
+      def self.compare(req_params, api)
+        req_def = find_request_definition api
+
+        diff = {}
+        req_def.each_pair do |param_name, definition|
+          is_body = definition[:body_param]
+          is_path = definition[:path_param]
+          is_req  = definition[:required]
+          data_type = definition[:type]
+
+          # find the particular parameter value
+          # if this is a body param, then use the whole request parameter object
+          param_val = is_body ? req_params : req_params[param_name]
+
+          if is_path
+            # ignore path parameter
+            next
+          elsif param_val.nil? && is_req
+            # parameter is missing but is required
+            diff[param_name] = :missing
+          else
+            result    = NovacastSDK::IdentityV1::Utils.type_check param_val, data_type
+
+            if is_body
+              # ignore not allowed parameter if this is body parameter
+              # because the all request params (i.e. the whole request.params) are included when checking body param
+              result.reject! { |k, v| v == :not_allowed } if is_body && !result.nil?
+
+              # merge the result to the top-level diff because
+              # body parameters go to the top-level params
+              diff.merge!(result) unless result.nil? || result.empty?
+            else
+              diff[param_name] = result unless result.nil? || result.empty?
+            end
+          end
+        end
+
+        diff.empty? ? nil : diff
+      end
+
+      private
+
+      def self.find_request_definition(api)
+        raise "Api '#{api}' request definition not found" unless (definition = REQUESTS[api.to_sym])
+        definition
+      end
+    end
+
     class ApiResponses
 
       RESPONSES = { 
-        :validate_token => {
+        validate_token: {
           
           200 => 'Account',
           
           0 => 'Error'
           
         }, 
-        :create_account => {
+        create_account: {
           
           201 => 'Account',
           
           0 => 'Error'
           
         }, 
-        :sign_in => {
+        sign_in: {
           
           200 => 'LoginResponse',
           
           0 => 'Error'
           
         }, 
-        :sign_out => {
+        sign_out: {
           
           200 => 'LogoutResponse',
           
           0 => 'Error'
           
         }, 
-        :get_account => {
+        get_account: {
           
           200 => 'Account',
           
           0 => 'Error'
           
         }, 
-        :get_account_permissions => {
+        get_account_permissions: {
           
           200 => 'ResourcePermissionsList',
           
           0 => 'Error'
           
         }, 
-        :validate_account_permission => {
+        validate_account_permission: {
           
           200 => 'InlineResponse200',
           
           0 => 'Error'
           
         }, 
-        :get_account_roles => {
+        get_account_roles: {
           
           200 => 'RoleResourcePermissionsList',
           
           0 => 'Error'
           
         }, 
-        :assign_account_role => {
+        assign_account_role: {
           
           200 => 'RoleResourcePermissions',
           
@@ -70,14 +512,14 @@ module NovacastSDK
           0 => 'Error'
           
         }, 
-        :get_account_user_roles => {
+        get_account_user_roles: {
           
           200 => 'AccessUserRoleList',
           
           0 => 'Error'
           
         }, 
-        :create_account_user_role => {
+        create_account_user_role: {
           
           200 => 'AccessUserRole',
           
@@ -86,133 +528,133 @@ module NovacastSDK
           0 => 'Error'
           
         }, 
-        :create_domain => {
+        create_domain: {
           
           201 => 'Domain',
           
           0 => 'Error'
           
         }, 
-        :get_domain_by_key => {
+        get_domain_by_key: {
           
           200 => 'Domain',
           
           0 => 'Error'
           
         }, 
-        :get_domain => {
+        get_domain: {
           
           200 => 'Domain',
           
           0 => 'Error'
           
         }, 
-        :create_access_permission => {
+        create_access_permission: {
           
           201 => 'AccessPermission',
           
           0 => 'Error'
           
         }, 
-        :update_access_permission => {
+        update_access_permission: {
           
           200 => 'AccessPermission',
           
           0 => 'Error'
           
         }, 
-        :delete_access_permission => {
+        delete_access_permission: {
           
           200 => 'AccessPermission',
           
           0 => 'Error'
           
         }, 
-        :create_access_resource => {
+        create_access_resource: {
           
           201 => 'AccessResource',
           
           0 => 'Error'
           
         }, 
-        :update_access_resource => {
+        update_access_resource: {
           
           200 => 'AccessResource',
           
           0 => 'Error'
           
         }, 
-        :delete_access_resource => {
+        delete_access_resource: {
           
           200 => 'AccessResource',
           
           0 => 'Error'
           
         }, 
-        :delete_role_permission => {
+        delete_role_permission: {
           
           200 => 'AccessRolePermission',
           
           0 => 'Error'
           
         }, 
-        :create_role_set => {
+        create_role_set: {
           
           201 => 'RoleSet',
           
           0 => 'Error'
           
         }, 
-        :get_role_set_by_key => {
+        get_role_set_by_key: {
           
           200 => 'RoleSet',
           
           0 => 'Error'
           
         }, 
-        :get_role_set => {
+        get_role_set: {
           
           200 => 'RoleSet',
           
           0 => 'Error'
           
         }, 
-        :get_roles => {
+        get_roles: {
           
           200 => 'AccessRoleList',
           
           0 => 'Error'
           
         }, 
-        :create_role => {
+        create_role: {
           
           201 => 'AccessRole',
           
           0 => 'Error'
           
         }, 
-        :update_role => {
+        update_role: {
           
           200 => 'AccessRole',
           
           0 => 'Error'
           
         }, 
-        :delete_access_role => {
+        delete_access_role: {
           
           200 => 'AccessRole',
           
           0 => 'Error'
           
         }, 
-        :get_role_permissions => {
+        get_role_permissions: {
           
           200 => 'AccessRolePermissionList',
           
           0 => 'Error'
           
         }, 
-        :create_role_permission => {
+        create_role_permission: {
           
           200 => 'AccessRolePermission',
           
@@ -221,7 +663,7 @@ module NovacastSDK
           0 => 'Error'
           
         }, 
-        :delete_user_role => {
+        delete_user_role: {
           
           200 => 'AccessUserRole',
           
@@ -234,8 +676,14 @@ module NovacastSDK
       def self.compare(resp_hash, api, status_code)
         resp_type = find_response_type api, status_code
 
-        NovacastSDK::Utils.type_check resp_hash, resp_type do |model_name|
-          NovacastSDK::IdentityV1::Models.const_get(model_name)
+        if resp_type.blank?
+          # if there is no response type, then the response body is expected to be empty
+          # if the body is not empty then return :invalid_type
+          resp_hash.nil? ? nil : :invalid_type
+        else
+          NovacastSDK::Utils.type_check resp_hash, resp_type do |model_name|
+            NovacastSDK::IdentityV1::Models.const_get(model_name)
+          end
         end
       end
 
@@ -271,9 +719,25 @@ module NovacastSDK
   end
 end
 
+RSpec::Matchers.define :be_api_request do |api|
+  match do |request_params|
+    @actual = request_params
+    @diff   = NovacastSDK::IdentityV1::ApiRequests.compare @actual, api
+    @diff.nil?
+  end
+
+  failure_message do
+    "expected request to be in #{api} format. \n\tDiff: #{@diff.to_s} \n\tactual: #{@actual} (please refer to api specification)"
+  end
+
+  failure_message_when_negated do
+    "expected reqeust to not be in #{api} format \n\tactual: #{@actual}"
+  end
+end
+
 RSpec::Matchers.define :be_api_response do |api, status|
   match do |response_body|
-    @actual = JSON.parse response_body
+    @actual = response_body.blank? ? nil : JSON.parse(response_body)
     @diff   = NovacastSDK::IdentityV1::ApiResponses.compare @actual, api, status
     @diff.nil?
   end
@@ -289,6 +753,12 @@ RSpec::Matchers.define :be_api_response do |api, status|
 end
 
 RSpec.shared_examples 'a novacast IdentityV1 sdk api' do |api, status, error_klass_name = nil, error_message = nil|
+  status_code = NovacastSDK::IdentityV1::ApiResponses.resolve_status(status)
+
+  it "accepts request in the format defined in api specification for #{api}" do
+    expect(request.params).to be_api_request(api)
+  end if status_code.between?(200, 299)
+
   it "responses in the format defined in api specification for #{api} (#{status})" do
     expect(response.body).to be_api_response(api, status)
   end
