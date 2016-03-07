756ed86b244bb6443cdb4e28f1ef9a76618e453e
lscspirit <lscspirit@hotmail.com>
2016-03-08 01:52:37 +0800

    New custom 'EnrollmentExists' error in EventV1

--- novacast-event-sdk/lib/novacast-event-sdk/errors.rb
+++ novacast-event-sdk/lib/novacast-event-sdk/errors.rb
@@ -3,6 +3,7 @@ module NovacastSDK
     module Errors
       class EventNotActive < NovacastSDK::Errors::Base; DEFAULT_STATUS=404; end
       class NoEventAccess < NovacastSDK::Errors::Base; DEFAULT_STATUS=401; end
+      class EnrollmentExists < NovacastSDK::Errors::Base; DEFAULT_STATUS=400; end
     end
   end
 end
