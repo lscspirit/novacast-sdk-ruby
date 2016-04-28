c67c475a33173cbc1260d40f8f8911c26b5a538e
lscspirit <lscspirit@hotmail.com>
2016-04-29 02:43:13 +0800

    A new NovacastSDK::Identity::permission_chain method for constructing permission chain.

--- novacast-sdk-common/lib/novacast-sdk-common/identity/base.rb
+++ novacast-sdk-common/lib/novacast-sdk-common/identity/base.rb
@@ -1,2 +1,26 @@
 require 'novacast-sdk-common/identity/definition'
 require 'novacast-sdk-common/identity/user_permissions'
+
+module NovacastSDK
+  module Identity
+    # Construct a permission map that has the specified permission at different resource levels
+    #
+    # @param permission [Symbol] a permission (see NovacastSDK::Identity::Definition::PERMISSIONS)
+    # @param channel_rn [String] resource name of a channel
+    # @param event_rn [String] resource name of a event
+    # @param session_rn [String] resource name of a session
+    #
+    # @example
+    #   permission_chain(:view_event, 'channel:1234', 'event:5678') #=> { 'channel:*' => :view_event, 'channel:1234' => :view_event, 'event:5678' => :view_event }
+    def self.permission_chain(permission, channel_rn = nil, event_rn = nil, session_rn = nil)
+      chain = {}
+
+      chain['channel:*'] = permission   # all channels
+      chain[channel_rn]  = permission if channel_rn
+      chain[event_rn]    = permission if event_rn
+      chain[session_rn]  = permission if session_rn
+
+      chain
+    end
+  end
+end
