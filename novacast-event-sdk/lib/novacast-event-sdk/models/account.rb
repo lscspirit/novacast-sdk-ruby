af5c57692603654ba5d105e283c1a2c0832ff2d1
lscspirit <lscspirit@hotmail.com>
2016-08-18 18:35:59 +0800

    Fixes the erroneous Account model caused by conflicting model definition in EventV1

--- novacast-event-sdk/lib/novacast-event-sdk/models/account.rb
+++ novacast-event-sdk/lib/novacast-event-sdk/models/account.rb
@@ -3,7 +3,7 @@ module NovacastSDK
     module Models
       # 
       class Account < NovacastSDK::BaseModel
-        attr_accessor :uid, :identifier
+        attr_accessor :uid, :identifier, :domain, :provider
 
         def self.api_model_module
           NovacastSDK::EventV1::Models
@@ -12,11 +12,17 @@ module NovacastSDK
         def self.model_properties
           {
             
-            # account unique id (uid)
+            # account unique id
             :'uid' => { base_name: 'uid', type: 'String', required: true },
             
             # account identifier (username or email)
-            :'identifier' => { base_name: 'identifier', type: 'String', required: true }
+            :'identifier' => { base_name: 'identifier', type: 'String', required: true },
+            
+            # domain name
+            :'domain' => { base_name: 'domain', type: 'String', required: true },
+            
+            # auth provider name
+            :'provider' => { base_name: 'provider', type: 'String', required: true }
             
           }
         end
